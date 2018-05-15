5124c6a23e8a6cad765ea6806e086d80a6052d15
JP S <github@shipherd.com>
2018-05-15 11:58:14 -0400

    Work in conjunction with webex API emulator and test cases here: https://github.com/jpjpjp/bot-test-framework-example

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -17,7 +17,7 @@ module.exports = function(controller) {
     var stats = {
         triggers: 0,
         convos: 0,
-    }
+    };
 
     controller.on('heard_trigger', function() {
         stats.triggers++;
@@ -43,17 +43,72 @@ module.exports = function(controller) {
 
     });
 
-    controller.hears(['^say (.*)','^say'], 'direct_message,direct_mention', function(bot, message) {
-        if (message.match[1]) {
+    controller.hears(['^/hello (.*)','^/hello'], 'direct_message,direct_mention', function(bot, message) {
+        // get users name out of bot object and build response
+        controller.api.people.get(message.data.personId).then(function(identity) {
+            let response = identity.displayName + ', you said hello to me!';
+            bot.reply(message, response);
+        }).catch(function(err) {
+            console.error('Failed looking up details for user:'+message.data.id+
+                ' Error: '+err.message);
+            bot.reply(message, 'Sorry. Can\'t figure out who you are!');
+        });
+    });
 
+    controller.hears(['^/echo (.*)','^/echo'], 'direct_message,direct_mention', function(bot, message) {
+        // echo back what the user entered after the /echo command
+        if (message.match[1]) {
             if (!wordfilter.blacklisted(message.match[1])) {
-                bot.reply(message, message.match[1]);
+                let response = "Ok, I'll say it: \""+message.match[1]+"\"";
+                bot.reply(message, response);
             } else {
                 bot.reply(message, '_sigh_');
             }
         } else {
-            bot.reply(message, 'I will repeat whatever you say.')
+            bot.reply(message, 'I will repeat whatever you say.');
         }
+    });
+
+    controller.hears(['^/whoami (.*)','^/whoami'], 'direct_message,direct_mention', function(bot, message) {
+        // get users info out of bot object and build response
+        controller.api.people.get(message.data.personId).then(function(person) {
+            controller.api.rooms.get(message.data.roomId).then(function(room) {
+                let response = `${person.displayName} here is some of your information: \n\n\n **Room:** you are in "**${room.title}**" \n\n\n **Room id:** *${message.data.roomId}*`;
+                bot.reply(message, response, function (err) {
+                    if (err) {console.error('Failed replying to /whoami: '+err.message);}
+                    response = ` **Email:** your email on file is *${person.emails[0]}*`;
+                    bot.reply(message, response);  
+                });
+            });
+        }).catch(function(err) {
+            console.error('Failed looking up details for user:'+message.data.id+
+                ' Error: '+err.message);
+            bot.reply(message, 'Sorry. Can\'t figure out who you are!');
+        });
+    });
+
+    controller.hears(['^/leave (.*)','^/leave'], 'direct_message,direct_mention', function(bot, message) {
+        // figure out how to send a message and then leave a room
+        bot.reply(message, "OK.  I know when I'm not wanted...");
+        controller.api.memberships.list({roomId: message.data.roomId}).then(function(memberships) {
+            let botId = message.createdBy; // Bot's user ID?
+            let botMembership = '';
+            for (i=0; i<memberships.items.length; i++) {
+                if (memberships.items[i].personId == botId) {
+                    botMembership = memberships.items[i].id;
+                    break;
+                }
+            }
+            if (botMembership) {
+                controller.api.memberships.remove(botMembership);
+            } else {
+                bot.reply(message, "but I don't know how to leave!");
+            }
+        }).catch(function(err) {
+            console.error('Failed looking up details for user:'+message.data.id+
+                ' Error: '+err.message);
+            bot.reply(message, 'Sorry. Can\'t figure out who how to leave!');
+        });
     });
 
 
